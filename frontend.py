6205dc4a23f9d9ca7c73b3979e64aee251450154
Harry Dixon-Hall <H.DIXON-HALL-2021@hull.ac.uk>
2025-10-28 12:13:54 +0000

    Added files for project components: simulation engine, controllers, frontend, middle layer.

--- frontend.py
+++ frontend.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash()
 
 # Requires Dash 2.17.0 or later
-app.layout = [html.Div(children='Hello World')]
+app.layout = html.Div([html.H2("CSTR Control Dashboard")])
 
 if __name__ == '__main__':
     app.run(debug=True)
